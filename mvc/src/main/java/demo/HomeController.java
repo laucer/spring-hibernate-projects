2de1a66d57078d731da19581d22d4fdfff87b524
laucer <lucjan.roslanowski@kontomatik.com>
2020-04-05 19:14:19 +0200

    Implementing mvc demo app

---
+++ mvc/src/main/java/demo/HomeController.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String showMyPage() {
+        System.out.print("I AM HERE");
+        return "main-menu";
+    }
+
+}
