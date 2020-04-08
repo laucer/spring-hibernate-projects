e4b6c4558bc581884eabd7eb3eac45d412ae2037
laucer <lucjan.roslanowski@kontomatik.com>
2020-04-08 22:23:42 +0200

    Builds cleaning, fetching config for mvc from application.properties

--- mvc/src/main/java/demo/HomeController.java
+++ mvc/src/main/java/demo/HomeController.java
@@ -2,13 +2,15 @@ package demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String showMyPage() {
-        return "main-menu";
+        System.out.println("A");
+        return "main";
     }
 
 }
